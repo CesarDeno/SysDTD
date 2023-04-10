92bd036ca12901414676ece428960f7bbc053a6c
Cesar Denogean <74329701+CesarDeno@users.noreply.github.com>
2023-04-10 13:44:48 -0700

    Respuestas en Base de Datos y Manejo de Sesiones
    Si ves esto tadeo probablemente nomas se pueda ejecutar en mi casa por como tengo el servidor y la configuracion de la base de datos, pero Ya guarda las respuestas en la base de datos y la guarda con el id del usuario que hizo el test

---
+++ SysDTD/login_usuario.php
@@ -0,0 +1,42 @@
+<?php
+
+$correo=$_POST['emailuser'];
+$pass=$_POST['pass'];
+
+// Datos de conexión a MySQL
+$host = "192.168.1.80";
+$user = "root";
+$password = "";
+$database = "sysdtd";
+
+// Conexión a MySQL
+$conn = new mysqli($host, $user, $password, $database);
+if ($conn->connect_error) {
+  die("Error de conexión: " . $conn->connect_error);
+}
+
+$consulta="SELECT*FROM usuarios where emailuser='$correo' and pass='$pass'";
+$resultado=mysqli_query($conn,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+$fila = mysqli_fetch_assoc($resultado);
+
+if($filas){
+    session_start();
+    $username = $fila['username'];
+    $usernameMAY = strtoupper($username);
+    $_SESSION['usuario'] = $usernameMAY;
+    $_SESSION['id'] = $fila['idUser'];
+    header("location:test.php");
+
+}else{
+    echo'
+        <script>
+            alert("Datos Incorrectos");
+            location.href="log-in.html";
+        </script>
+    ';
+}
+
+mysqli_free_result($resultado);
+mysqli_close($conn);
